359069e853a72543a14d8097f0f1fc9c90652a96
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2022-10-31 11:09:16 +0100

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const assets = require("../src/assets");
 
 function App() {
+  const obj = {
+    ears: assets.ears.default,
+    nose: assets.nose.nose,
+    legs: assets.legs.default,
+    accessories: assets.accessories.headphones,
+    backgrounds: assets.backgrounds.blue50,
+    mouths: assets.mouths.default,
+    necks: assets.necks.default,
+    eyes: assets.eyes.default,
+    hairs: assets.hairs.default,
+  };
+
+  const [setUp, setSetUp] = useState(obj);
+  const [setting, setSetting] = useState(assets["accessories"]);
+
+  const {
+    ears,
+    nose,
+    legs,
+    accessories,
+    backgrounds,
+    mouths,
+    necks,
+    eyes,
+    hairs,
+  } = setUp;
+
+  const buttonList = [
+    "Accessories",
+    "Backgrounds",
+    "Ears",
+    "Eyes",
+    "Hairs",
+    "Legs",
+    "Mouths",
+    "Necks",
+  ];
+
+  const handleMain = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const equi = Object.keys(assets).filter((ele) => name === ele);
+    setSetting(assets[equi]);
+  };
+
+  const handlePart = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const select = Object.keys(assets).filter((sel) => assets[sel] === setting);
+    const selected = select.toString();
+    setSetUp((prev) => {
+      return { ...prev, [selected]: setting[name] };
+    });
+  };
+
+  const handleRandom = () => {
+    Object.keys(assets).map((image) => {
+      const randomIndex = Math.floor(
+        Math.random() * Object.values(assets[image]).length
+      );
+
+      const randomPart = Object.values(assets[image])[randomIndex];
+
+      setSetUp((prev) => {
+        return { ...prev, [image]: randomPart };
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <main
+        className="container"
+        style={{
+          backgroundImage: `url('${backgrounds}')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "cover",
+          cursor: "pointer",
+        }}
+      >
+        <section className="section">
+          <div className="image_section">
+            <img className="accessory" src={accessories} alt="accessory" />
+            <img className="ear" src={ears} alt="ear" />
+            <img className="hair" src={hairs} alt="hair" />
+            <img className="eye" src={eyes} alt="eye" />
+            <img className="neck" src={necks} alt="neck" />
+            <img className="mouth" src={mouths} alt="mouth" />
+            <img className="nose" src={nose} alt="noses" />
+            <img className="leg" src={legs} alt="leg" />
+          </div>
+          <div className="button">
+            <button onClick={handleRandom}>Random</button>
+            <button>Download</button>
+          </div>
+        </section>
+        <section className="section">
+          <div className="button_section">
+            <div className="button">
+              {buttonList.map((button) => (
+                <button
+                  onClick={handleMain}
+                  value={button.toLowerCase()}
+                  name={button.toLowerCase()}
+                >
+                  {button}
+                </button>
+              ))}
+            </div>
+            <div className="button">
+              {Object.keys(setting).map((but) => (
+                <button name={but} value={but} onClick={handlePart}>
+                  {but}
+                </button>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/assets.js
@@ -0,0 +1,81 @@
+const accessories= {
+    earings: require("./alpaca/accessories/earings.png"),
+    flower: require("./alpaca/accessories/flower.png"),
+    glasses: require("./alpaca/accessories/glasses.png"),
+    headphones: require("./alpaca/accessories/headphone.png"),
+  }
+
+const backgrounds= {
+    blue50: require("./alpaca/backgrounds/blue50.png"),
+    blue60: require("./alpaca/backgrounds/blue60.png"),
+    blue70: require("./alpaca/backgrounds/blue70.png"),
+    darkblue30: require("./alpaca/backgrounds/darkblue30.png"),
+    darkblue50: require("./alpaca/backgrounds/darkblue50.png"),
+    darkblue70: require("./alpaca/backgrounds/darkblue70.png"),
+    green50: require("./alpaca/backgrounds/green50.png"),
+    green60: require("./alpaca/backgrounds/green60.png"),
+    green70: require("./alpaca/backgrounds/green70.png"),
+    grey40: require("./alpaca/backgrounds/grey40.png"),
+    grey70: require("./alpaca/backgrounds/grey70.png"),
+    grey80: require("./alpaca/backgrounds/grey80.png"),
+    red50: require("./alpaca/backgrounds/red50.png"),
+    red60: require("./alpaca/backgrounds/red60.png"),
+    red70: require("./alpaca/backgrounds/red70.png"),
+    yellow50: require("./alpaca/backgrounds/yellow50.png"),
+    yellow60: require("./alpaca/backgrounds/yellow60.png"),
+   yellow70: require("./alpaca/backgrounds/yellow70.png"),
+  }
+
+const ears = {
+    default: require("./alpaca/ears/default.png"),
+    tiltBackward: require("./alpaca/ears/tilt-backward.png"),
+    tiltForward:require("./alpaca/ears/tilt-forward.png"),
+  }
+
+const eyes = {
+  angry: require("./alpaca/eyes/angry.png"),
+  default: require("./alpaca/eyes/default.png"),
+  naughty: require("./alpaca/eyes/naughty.png"),
+  panda: require("./alpaca/eyes/panda.png"),
+  smart: require("./alpaca/eyes/smart.png"),
+  star: require("./alpaca/eyes/star.png")
+}
+const hairs = {
+  bang: require("./alpaca/hair/bang.png"),
+  curls: require("./alpaca/hair/curls.png"),
+  default: require("./alpaca/hair/default.png"),
+  elegant: require("./alpaca/hair/elegant.png"),
+  fancy: require("./alpaca/hair/fancy.png"),
+  quiff: require("./alpaca/hair/quiff.png"),
+  short: require("./alpaca/hair/short.png")
+}
+
+const legs = {
+  bubbleTea: require('./alpaca/leg/bubble-tea.png'),
+  cookie: require('./alpaca/leg/cookie.png'),
+  default: require('./alpaca/leg/default.png'),
+  gameConsole: require('./alpaca/leg/game-console.png'),
+  tiltBackward: require('./alpaca/leg/tilt-backward.png'),
+  tiltForward: require('./alpaca/leg/tilt-forward.png')
+}
+
+const mouths = {
+  astonished: require('./alpaca/mouth/astonished.png'),
+  default: require('./alpaca/mouth/default.png'),
+  eating: require('./alpaca/mouth/eating.png'),
+  laugh: require('./alpaca/mouth/laugh.png'),
+  tongue: require('./alpaca/mouth/tongue.png')
+}
+
+const necks= {
+  bendBackward: require('./alpaca/neck/bend-backward.png'),
+  bendForward: require('./alpaca/neck/bend-forward.png'),
+  default: require('./alpaca/neck/default.png'),
+  thick: require('./alpaca/neck/thick.png')
+}
+
+const nose = {nose: require('./alpaca/nose.png')}
+
+const assets = {accessories, backgrounds, ears, legs, mouths, necks, nose, eyes, hairs}
+
+module.exports = assets
